feff177b07664049a442ef13f35bb1bfe8198df6
Wilcson Araújo <32008067+wilcsonaraujo@users.noreply.github.com>
2021-11-09 13:50:30 -0300

    primeiro

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Strip-packing-problem
+
+## Configuração
+
+O algoritimo está em Python.
+Para rodá-lo é necessário a instalação do python na versão mais recente e do pip.
+
+Instalar a biblioteca de gráfico ´´´matplotlib´´´ 
---
+++ exemplo.py
@@ -0,0 +1,21 @@
+from spp.ph import phspprg, phsppog
+from spp import visualize
+from collections import namedtuple
+
+Rectangle = namedtuple('Rectangle', ['x', 'y', 'w', 'h'])
+
+
+def main():
+    boxes = [
+        [5, 3], [5, 3], [2, 4], [30, 8], [10, 20],
+        [20, 10], [5, 5], [5, 5], [10, 10], [10, 5],
+        [6, 4], [1, 10], [8, 4], [6, 6], [20, 14]
+    ]
+    width = 30
+    height, rectangles = phspprg(width, boxes)
+    visualize(width, height, rectangles)
+    print("The height is: {}".format(height))
+
+
+if __name__ == "__main__":
+    main()
